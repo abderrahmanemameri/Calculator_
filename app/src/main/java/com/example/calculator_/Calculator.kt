cc7eecebd386a4500e82457333367af6f3437e53
abderrahmanemameri <abdoumam0405@gmail.com>
2025-11-29 14:46:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/calculator_/Calculator.kt
@@ -0,0 +1,99 @@
+package com.example.calculator_
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val button_list = listOf(
+    "C","(",")","/",
+    "7","8","9","*",
+    "4","5","6","+",
+    "1","2","3","-",
+    "AC","0",".","=",
+)
+
+@Composable
+fun Calculator(modifier: Modifier= Modifier)
+{
+    Box(modifier = Modifier.background(Color.White)) {
+        Column (modifier = Modifier.fillMaxSize().padding(top = 20.dp, end = 10.dp),
+            horizontalAlignment = Alignment.End)
+        {
+            Text(
+                text = "123+456",
+
+                textAlign = TextAlign.End,
+                fontSize = 30.sp,
+                color = Color.Black,
+                maxLines = 5,
+                overflow = TextOverflow.Ellipsis
+
+            )
+            Text(
+                text = "123+456",
+
+                textAlign = TextAlign.End,
+                fontSize = 60.sp,
+                color = Color.Black,
+                maxLines = 2,
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+
+            LazyVerticalGrid( columns = GridCells.Fixed(4),     verticalArrangement = Arrangement.spacedBy(12.dp),
+
+                )
+            {
+                items(button_list){
+               ButtonCalculator(it)
+                }
+
+
+            }
+
+        }
+
+
+
+
+    }
+
+
+
+}
+@Composable
+fun ButtonCalculator(btn : String) {
+Box (modifier = Modifier.padding(start = 10.dp))
+{
+    FloatingActionButton(onClick = {}, modifier = Modifier, shape = CircleShape) {
+        Text(text = btn)
+
+    }
+
+
+}
+
+}

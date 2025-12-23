55d96ad0f7592e3b8ea8a573556535bb902c7228
abderrahmanemameri <abdoumam0405@gmail.com>
2025-12-23 14:22:19 +0100

    Initial commit

--- app/src/main/java/com/example/calculator_/calculaterViewModel.kt
+++ app/src/main/java/com/example/calculator_/calculaterViewModel.kt
@@ -1,34 +1,43 @@
 package com.example.calculator_
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class calculaterViewModel : ViewModel() {
-    private val _equation= MutableLiveData("")
-    val equation : LiveData<String> =_equation
+    private var _equation by mutableStateOf("")
+        private set
 
-    private val _result= MutableLiveData("0")
-    val result: LiveData<String> =_result
+     private var _result by mutableStateOf("0")
+        private set
+
+
     fun onButtonClick(btn: String)
     {
         Log.i("clicked Button",btn)
-       _equation.value?.let{
+       _equation?.let{
             if (btn=="AC"){
-                _equation.value=""
-                _result.value="0"
+                _equation=""
+                _result="0"
                 return
             }
            if (btn=="C"){
-               if (it.isNotEmpty()){
-                   _equation.value=it.substring(0,it.length-1)
-                    return
+               if (_equation.isNotEmpty()){
+                   _equation=_equation.dropLast(1)
+
                }
-
-
+               return
            }
-
+           if (btn=="="){
+                _equation=_result
+               return
+           }
+            _equation=_equation+btn
+           
         }
 
     }
